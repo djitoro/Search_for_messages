e5dcd4a4d58652942ed4a89c5077f8d88cfc2754
Admin <dimaihnenko@gmail.com>
2024-08-17 13:44:58 +0300

    Add model hypothesizing, splitting data into tokens, creating a model (parameters are not exact)

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import pandas as pd
-import numpy as np
+# Hypotheses:
+# 1. Use words as toxins and count the scalar product
 
-with open('corpus.txt', 'r', encoding='utf-8') as file:
-    data: str = file.read()
+# import pandas as pd
+# import numpy as np
+from gensim.models import Word2Vec
+import re
 
-print(data[1:100:1])
+
+with open('corpus.txt', 'r', encoding='utf-8') as f:
+    sentences = f.readlines()
+
+
+def tokenize(text):
+    return re.findall(r'\w+|[^\w\s]', text, re.UNICODE)
+
+
+sentences_tokenized = [tokenize(sentence) for sentence in sentences]
+
+model = Word2Vec(sentences=sentences_tokenized,
+                 vector_size=300,
+                 window=5,
+                 min_count=5,
+                 workers=4)
